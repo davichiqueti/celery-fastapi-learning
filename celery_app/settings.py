bc7cbca752c311fcae4eb4bec7a64695efa45ca4
Davi Chiqueti <davichiqueti@gmail.com>
2024-05-23 15:10:05 -0300

    Update: Change Settings to Celery project pattern

--- celery_app/settings.py
+++ celery_app/settings.py
@@ -4,18 +4,15 @@ import os
 
 
 load_dotenv()
-
-
-class BaseSettings:
-    """Celery Configuration"""
-    broker_url = os.getenv('CELERY_BROKER_URL')
-    result_backend = None
-    broker_transport = "sqs"
-    task_default_queue = 'default'
-    task_queues = (
-        Queue("scraping-queue"),
-    )
-    task_routes = {
-        'tasks.scrape_offer': {'queue': 'scraping-queue'},
-    }
-    inlude: list = ['tasks']
+broker_url = os.getenv('CELERY_BROKER_URL')
+result_backend = None
+broker_transport = "sqs"
+task_default_queue = 'default'
+concurrency = 8
+task_queues = (
+    Queue("scraping-queue"),
+)
+task_routes = {
+    'tasks.scrape_offer': {'queue': 'scraping-queue'},
+}
+inlude: list = ['tasks']
